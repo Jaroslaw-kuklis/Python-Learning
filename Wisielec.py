9f36cfb8e991501ecc577efc9ad9b3197df25e1b
unknown <jaroslaw.kuklis@gmail.com>
2022-08-02 00:15:00 +0200

    adding print def

--- Wisielec.py
+++ Wisielec.py
@@ -17,6 +17,12 @@ def find_indexes(word, letter):
             indexes.append(index)
     return indexes
 
+def show_state_of_game():
+    print()
+    print(user_word)
+    print("Number of tries:", num_of_tries)
+    print("Used letters:", used_letters)
+
 
 for _ in word:
     user_word.append("_")
@@ -29,9 +35,18 @@ while True:
     if len(found_indexes) == 0:
         print("Wrong letter")
         num_of_tries -= 1
-        print("Left number of tries:", num_of_tries)
+        
         if num_of_tries == 0:
             print("The end of game")
             sys.exit(0)
+    else:
+        for index in found_indexes:
+            user_word[index] = letter
+        
 
-    print("used letters:", used_letters)
+        if "".join(user_word) == word:
+            print("You find the correct word!")
+            sys.exit(0)
+    show_state_of_game()
+
+    
